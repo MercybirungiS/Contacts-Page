b6e043873e08ee23ea69d03eb9a91cf641197b87
MercybirungiS <mercyssbirungi@gmail.com>
2021-06-19 23:47:56 +0300

    page

--- app/src/main/java/com/example/contactspage/MainActivity.kt
+++ app/src/main/java/com/example/contactspage/MainActivity.kt
@@ -4,14 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var button=findViewById<Button>(R.id.btnContact)
-        button.setOnClickListener {
+        var imageView=findViewById<ImageView>(R.id.ivContacts)
+        imageView.setOnClickListener {
             var intent=Intent(baseContext,MainActivity2::class.java)
             startActivity(intent)
         }
